41cd04985cc9572b9c644e7c56736ee6fe72dd5c
Vaibhav Rahangdale <vaibhavrahangdale1234@gmail.com>
2026-05-12 16:17:58 +0530

    first docker implementation

---
+++ src/main/java/com/demo/DockersPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockersPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockersPracticeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/demo/controller/PracticeController.java
@@ -0,0 +1,17 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PracticeController {
+	
+	 @GetMapping("/test")
+	    public String getMessage(){
+	        System.out.println("##########################");
+	        return "Hello World from psa";
+	    }
+
+}
